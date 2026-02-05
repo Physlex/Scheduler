30a143632092e0af9f461ff30bfd77923e72604c
alexander <al.willet02@gmail.com>
2026-02-04 20:59:21 -0700

    task: Working on proc macro integration

--- lib-macros/src/executor.cpp
+++ lib-macros/src/executor.cpp
@@ -1,60 +1,40 @@
-/** @file `executor.cpp`
- *  @brief TODO: DOCS
- */
+// Declares clang::SyntaxOnlyAction.
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+// Declares llvm::cl::extrahelp.
+#include "llvm/Support/CommandLine.h"
 
-#include <clang/AST/ASTConsumer.h>
-#include <clang/AST/RecursiveASTVisitor.h>
-#include <clang/Frontend/CompilerInstance.h>
-#include <clang/Frontend/FrontendAction.h>
-#include <clang/Tooling/Tooling.h>
+using namespace clang::tooling;
+using namespace llvm;
 
-using namespace clang;
+// Apply a custom category to all command-line options so that they are the
+// only ones displayed.
+static llvm::cl::OptionCategory MyToolCategory("my-tool options");
 
-class FindNamedClassVisitor
-    : public RecursiveASTVisitor<FindNamedClassVisitor> {
-   public:
-    explicit FindNamedClassVisitor(ASTContext* Context) : Context(Context) {}
+// CommonOptionsParser declares HelpMessage with a description of the common
+// command-line options related to the compilation database and input files.
+// It's nice to have this help message in all tools.
+static cl::extrahelp CommonHelp(CommonOptionsParser::HelpMessage);
 
-    bool VisitCXXRecordDecl(CXXRecordDecl* Declaration) {
-        if (Declaration->getQualifiedNameAsString() == "n::m::C") {
-            FullSourceLoc FullLocation =
-                Context->getFullLoc(Declaration->getBeginLoc());
-            if (FullLocation.isValid())
-                llvm::outs() << "Found declaration at "
-                             << FullLocation.getSpellingLineNumber() << ":"
-                             << FullLocation.getSpellingColumnNumber() << "\n";
-        }
-        return true;
-    }
+// A help message for this specific tool can be added afterwards.
+static cl::extrahelp MoreHelp("\nMore help text...\n");
 
-   private:
-    ASTContext* Context;
-};
+int main(int argc, const char** argv) {
+    // auto ExpectedParser =
+    //     CommonOptionsParser::create(argc, argv, MyToolCategory);
+    // if (!ExpectedParser) {
+    //     // Fail gracefully for unsupported options.
+    //     llvm::errs() << ExpectedParser.takeError();
+    //     return 1;
+    // }
 
-class FindNamedClassConsumer : public clang::ASTConsumer {
-   public:
-    explicit FindNamedClassConsumer(ASTContext* Context) : Visitor(Context) {}
+    // CommonOptionsParser& OptionsParser = ExpectedParser.get();
+    // auto compilations = OptionsParser.getCompilations();
+    // auto sourcePathList = OptionsParser.getSourcePathList();
 
-    virtual void HandleTranslationUnit(clang::ASTContext& Context) {
-        Visitor.TraverseDecl(Context.getTranslationUnitDecl());
-    }
+    // auto syntaxOnlyAction = newFrontendActionFactory
+    //     <clang::SyntaxOnlyAction>().get();
 
-   private:
-    FindNamedClassVisitor Visitor;
-};
-
-class FindNamedClassAction : public clang::ASTFrontendAction {
-   public:
-    virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
-        clang::CompilerInstance& Compiler, llvm::StringRef InFile) {
-        return std::make_unique<FindNamedClassConsumer>(
-            &Compiler.getASTContext());
-    }
-};
-
-int main(int argc, char** argv) {
-    if (argc > 1) {
-        clang::tooling::runToolOnCode(std::make_unique<FindNamedClassAction>(),
-                                      argv[1]);
-    }
+    // return ClangTool(compilations, sourcePathList).run(syntaxOnlyAction);
 }
