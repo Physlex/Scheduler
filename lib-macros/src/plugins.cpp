b46c8fe55fdd00732a471b572ccb3deb96f8582b
alexander <al.willet02@gmail.com>
2026-03-08 18:57:35 -0600

    feat: Can now print attributes off of each function and print them

--- lib-macros/src/plugins.cpp
+++ lib-macros/src/plugins.cpp
@@ -6,29 +6,75 @@
 #include "clang/AST/ASTConsumer.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendPluginRegistry.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
 
 using namespace clang;
 using namespace clang::attr;
+using namespace clang::ast_matchers;
 using namespace llvm;
 
-using namespace clang;
-
-class HelloConsumer : public ASTConsumer {
-  std::string FileName;
-public:
-  explicit HelloConsumer(std::string FileName) : FileName(std::move(FileName)) {}
-
-  void HandleTranslationUnit(ASTContext &Context) override {
-    llvm::outs() << "Hello, " << FileName << "\n";
-  }
+/**
+ *  @brief TODO: DOCS
+ */
+class AnnotatedCallbackHandler : public MatchFinder::MatchCallback {
+  public:
+    //! @brief TODO: DOCS
+    void run(const MatchFinder::MatchResult &Result) override {
+        const FunctionDecl *FD = Result.Nodes.getNodeAs<FunctionDecl>("funcDecl");
+        if (FD) {
+            if (FD->hasBody()) {
+                llvm::outs() << "Function: " << FD->getNameAsString() << " has attributes:\n";
+                for (auto attr : FD->getAttrs()) {
+                    // Check for specific attribute types, e.g., AnnotateAttr
+                    if (const AnnotateAttr *Annot = dyn_cast<AnnotateAttr>(attr)) {
+                        llvm::outs() << "  Annotate attribute: " << Annot->getAnnotation() << "\n";
+                    } else {
+                        // For general attributes, you can print their kind or spelling
+                        llvm::outs() << "  Attribute kind ID: " << attr->getKind() << "\n";
+                        // Or print a pretty string representation
+                        std::string SS;
+                        llvm::raw_string_ostream S(SS);
+                        PrintingPolicy Policy(FD->getLangOpts());
+                        attr->printPretty(S, Policy);
+                        llvm::outs() << "  Attribute string: " << S.str() << "\n";
+                    }
+                }
+            }
+        }
+    }
 };
 
-class HelloAction : public PluginASTAction {
+/**
+ *  @brief TODO: DOCS
+ */
+class ProcMacroConsumer : public ASTConsumer {
+  public:
+    ProcMacroConsumer(std::string FileName) : FileName(std::move(FileName)) {}
+
+    void HandleTranslationUnit(ASTContext &Context) override {
+        AnnotatedCallbackHandler Handler;
+        MatchFinder Finder;
+
+        // Match any function declaration
+        Finder.addMatcher(
+            functionDecl().bind("funcDecl"),
+            &Handler
+        );
+
+        Finder.matchAST(Context);
+    }
+
+  private:
+    std::string FileName;  
+};
+
+class ProcMacroPlugin : public PluginASTAction {
   protected:
     std::unique_ptr<ASTConsumer> CreateASTConsumer(
         CompilerInstance &CI, llvm::StringRef InFile
     ) override {
-        return std::make_unique<HelloConsumer>(InFile.str());
+        return std::make_unique<ProcMacroConsumer>(InFile.str());
     }
 
     bool ParseArgs(
@@ -42,5 +88,5 @@ class HelloAction : public PluginASTAction {
     }
 };
 
-static FrontendPluginRegistry::Add<HelloAction>
-    X("hello-plugin", "Prints Hello, <file_name>");
+static FrontendPluginRegistry::Add<ProcMacroPlugin>
+    X("gbox-macros-plugin", "Procedural macro scheme for C/C++");
